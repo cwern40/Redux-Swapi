15a73400eca0ae5a6ec24c2c20cc8fbd9ed85ecc
cwern40 <chris.wernli@hotmail.com>
2019-07-16 18:20:08 -0600

    I added in my reducers for the axios actions

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -5,6 +5,20 @@ const initialState = {
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case FETCHING: {
+      return {
+        ...state,
+        isLoading: true,
+      }
+    }
+    case SUCCESS: {
+      return {
+        state
+      }
+    } 
+    case FAILURE: {
+      return action.payload
+    }
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
